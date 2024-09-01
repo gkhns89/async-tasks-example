d9d55c9ee147507edc02e36aacf025c00b44f394
Gökhan <gokhansisman@outlook.com>
2024-09-01 03:54:33 +0300

    Main Commit

---
+++ Tasks Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TasksExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Main Method is started.");
+
+            Task.Factory.StartNew(() => Works.Work3());
+            Task.Factory.ContinueWhenAll(
+          
+                new Task[]
+            {
+                        Task.Factory.StartNew(() => Works.Work4()),
+                        Task.Factory.StartNew(() => Works.Work2())
+            
+            },
+
+            tasks=>Works.Work1()
+            
+            );
+            
+
+            Console.WriteLine("Main Method done.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tasks Example/Works.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace TasksExample
+{
+    class Works
+    {
+       public static void Work1()
+        {
+            Console.WriteLine("Work 1 is started.");
+            Thread.Sleep(2000);
+            Console.WriteLine("Work 1 done.");
+        }
+       
+       public static void Work2()
+        {
+            Console.WriteLine("Work 2 is started.");
+            Thread.Sleep(10000);
+            Console.WriteLine("Work 2 done.");
+        }
+       
+       public static void Work3()
+        {
+            Console.WriteLine("Work 3 is started.");
+            Thread.Sleep(15000);
+            Console.WriteLine("Work 3 done.");
+        }
+       
+       public static void Work4()
+        {
+            Console.WriteLine("Work 4 is started.");
+            Thread.Sleep(10000);
+            Console.WriteLine("Work 4 done.");
+        }
+    }
+}
